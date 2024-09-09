909b1863685df3780f8dc266636187b8062c2750
vskelton <vskelton25@gmail.com>
2024-09-09 12:48:36 -0400

    continuing to update js file with jQuery

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function newItem()
 let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
+
+if (inputValue === '') {
+    alert('You must write something!');
+} else {
+    $('#list').append(li);
+}
