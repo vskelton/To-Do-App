b8b1756f6753b3761c0edbd9d4703d039aec45ed
vskelton <vskelton25@gmail.com>
2024-09-10 09:54:51 -0400

    Update JS file
    added delete button and cross out item

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function newItem()
+function newItem() {
 
 //1. Adding a new item to the list of items:
 let li = $('<li></li>');
@@ -9,4 +9,24 @@ if (inputValue === '') {
     alert('You must write something!');
 } else {
     $('#list').append(li);
+}
+
+//2. Crossing out an item
+function crossOut() {
+    li.toggleClass('strike');
+}
+
+li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+});
+
+//3. Adding a delete button
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on('click', deleteListItem);
+function deleteListItem() {
+    li.addClass('delete')
+}
 }
